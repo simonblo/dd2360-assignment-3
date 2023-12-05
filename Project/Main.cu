83115d4ecf108c251470356641fac78088454f26
Simon Blom <102825244+simonblo@users.noreply.github.com>
2023-12-05 12:34:13 +0100

    Complete exercise 1

---
+++ Project/Main.cu
@@ -0,0 +1,11 @@
+﻿﻿#include <cuda.h>
+#include <cuda_runtime_api.h>
+#include <device_launch_parameters.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	return 0;
+}
