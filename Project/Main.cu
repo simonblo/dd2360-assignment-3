a730e3b2de8a6ea9a36692cbc8d1b28d7415ca6b
Simon Blom <102825244+simonblo@users.noreply.github.com>
2023-12-09 14:40:05 +0100

    Complete exercise 1

--- Project/Main.cu
+++ Project/Main.cu
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <time.h>
 
-#define A (1<<25)
+#define N (1<<25)
 #define B 4096
 
 typedef unsigned int uint32;
@@ -50,42 +50,32 @@ int main()
 {
 	uint32* cpuBuffer1;
 	uint32* cpuBuffer2;
-	uint32* cpuBuffer3;
 	uint32* gpuBuffer1;
 	uint32* gpuBuffer2;
 
-	srand(time(NULL));
-
-	cpuBuffer1 = (uint32*)malloc(A * sizeof(uint32));
-	cpuBuffer2 = (uint32*)malloc(B * sizeof(uint32));
-	cpuBuffer3 = (uint32*)malloc(B * sizeof(uint32));
-
-	//cudaHostAlloc((void**)&cpuBuffer1, A * sizeof(uint32), cudaHostAllocDefault);
-	//cudaHostAlloc((void**)&cpuBuffer2, B * sizeof(uint32), cudaHostAllocDefault);
-	//cudaHostAlloc((void**)&cpuBuffer3, B * sizeof(uint32), cudaHostAllocDefault);
-
-	memset(cpuBuffer3, 0, B * sizeof(uint32));
-
-	for (int i = 0; i != A; ++i)
-	{
-		uint32 v      = (uint32)rand() % (uint32)B;
-		cpuBuffer1[i] = v;
-		cpuBuffer3[v] = cpuBuffer3[v] + (cpuBuffer3[v] < 127);
-	}
-
-	cudaMalloc((void**)&gpuBuffer1, A * sizeof(uint32));
-	cudaMalloc((void**)&gpuBuffer2, B * sizeof(uint32));
-
-	uint32 errors  = 0;
 	uint32 threads = 0;
 	uint32 blocks  = 0;
+	uint32 errors  = 0;
 
-	cudaMemcpy(gpuBuffer1, cpuBuffer1, A * sizeof(uint32), cudaMemcpyHostToDevice);
+	srand(time(NULL));
+
+	cudaHostAlloc((void**)&cpuBuffer1, N * sizeof(uint32), cudaHostAllocDefault);
+	cudaHostAlloc((void**)&cpuBuffer2, B * sizeof(uint32), cudaHostAllocDefault);
+
+	cudaMalloc((void**)&gpuBuffer1, N * sizeof(uint32));
+	cudaMalloc((void**)&gpuBuffer2, B * sizeof(uint32));
+
+	for (int i = 0; i != N; ++i)
+	{
+		cpuBuffer1[i] = (uint32)rand() % (uint32)B;
+	}
+
+	cudaMemcpy(gpuBuffer1, cpuBuffer1, N * sizeof(uint32), cudaMemcpyHostToDevice);
 	cudaMemset(gpuBuffer2, 0, B * sizeof(uint32));
 
-	threads = 64;
-	blocks  = (A + threads - 1) / threads;
-	gpuHistogram<<<blocks, threads>>>(gpuBuffer1, A, gpuBuffer2, B);
+	threads = 1024;
+	blocks  = (N + threads - 1) / threads;
+	gpuHistogram<<<blocks, threads>>>(gpuBuffer1, N, gpuBuffer2, B);
 
 	threads = 64;
 	blocks  = (B + threads - 1) / threads;
@@ -94,24 +84,27 @@ int main()
 	cudaDeviceSynchronize();
 	cudaMemcpy(cpuBuffer2, gpuBuffer2, B * sizeof(uint32), cudaMemcpyDeviceToHost);
 
-	for (int i = 0; i != B; ++i)
+	uint32 values[B];
+	memset(values, 0, B * sizeof(uint32));
+
+	for (int i = 0; i != N; ++i)
 	{
-		errors += (cpuBuffer2[i] != cpuBuffer3[i]);
+		values[cpuBuffer1[i]] += values[cpuBuffer1[i]] < 127 ? 1 : 0;
 	}
 
-	printf("Elements: %u\n", A);
+	for (int i = 0; i != B; ++i)
+	{
+		errors += (cpuBuffer2[i] != values[i]);
+	}
+
+	printf("Elements: %u\n", N);
 	printf("Errors:   %u\n", errors);
 
 	cudaFree(gpuBuffer1);
 	cudaFree(gpuBuffer2);
 
-	free(cpuBuffer1);
-	free(cpuBuffer2);
-	free(cpuBuffer3);
-
-	//cudaFreeHost(cpuBuffer1);
-	//cudaFreeHost(cpuBuffer2);
-	//cudaFreeHost(cpuBuffer3);
+	cudaFreeHost(cpuBuffer1);
+	cudaFreeHost(cpuBuffer2);
 
 	return 0;
 }
